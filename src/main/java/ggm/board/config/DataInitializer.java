51744277d681e03c9a7517d1429f499dfc718fca
bjh6654 <bjh6654@naver.com>
2025-12-26 01:22:26 +0900

    Add DataInitializer to create dummy data set.

---
+++ src/main/java/ggm/board/config/DataInitializer.java
@@ -0,0 +1,125 @@
+package ggm.board.config;
+
+import com.github.javafaker.Faker;
+import ggm.board.domain.auth.entity.Auth;
+import ggm.board.domain.auth.entity.UserRole;
+import ggm.board.domain.auth.repository.AuthRepository;
+import ggm.board.domain.member.entity.Member;
+import ggm.board.domain.member.repository.MemberUserRepository;
+import ggm.board.domain.post.entity.Post;
+import ggm.board.domain.post.entity.PostContent;
+import ggm.board.domain.post.entity.Reply;
+import ggm.board.domain.post.repository.PostContentRepository;
+import ggm.board.domain.post.repository.PostRepository;
+import ggm.board.domain.post.repository.ReplyRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class DataInitializer {
+    private final Faker faker = new Faker(new Locale("ko"));
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+    @Bean
+    public CommandLineRunner initializeData(
+            MemberUserRepository memberUserRepository,
+            AuthRepository authRepository,
+            PostRepository postRepository,
+            PostContentRepository postContentRepository,
+            ReplyRepository replyRepository) {
+
+        return args -> {
+            // 사용자 생성
+            List<Member> memberUsers = new ArrayList<>();
+            List<Auth> auths = new ArrayList<>();
+            for (int i = 0; i < 20; i++) {
+                Member user = Member.builder()
+                        .name(faker.name().username())
+                        .build();
+                memberUsers.add(memberUserRepository.save(user));
+
+                Auth auth = Auth.builder()
+                        .authMember(user)
+                        .email(faker.internet().emailAddress())
+                        .password(bCryptPasswordEncoder.encode(faker.internet().password()))
+                        .role(UserRole.USER)
+                        .build();
+                auths.add(authRepository.save(auth));
+//                MemberAuth auth = MemberAuth.builder()
+//                        .email(faker.internet().emailAddress())
+//                        .type("naver")
+//                        .authMember(user)
+//                        .build();
+//                memberAuths.add(memberAuthRepository.save(auth));
+            }
+            Member user = Member.builder()
+                    .name("박진혁")
+                    .build();
+            memberUsers.add(memberUserRepository.save(user));
+
+            auths.add(authRepository.save(Auth.builder()
+                    .authMember(user)
+                    .email("admin1065@naver.com")
+                    .password(bCryptPasswordEncoder.encode("admin123!@#"))
+                    .role(UserRole.USER)
+                    .build())
+            );
+
+            // 게시글 생성
+            List<Post> posts = new ArrayList<>();
+            List<PostContent> postContents = new ArrayList<>();
+            Date from = new Date(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(365 * 2)); // 약 2년 전
+            Date to = new Date(System.currentTimeMillis());
+            for (int i = 0; i < 80; i++) {
+                Post post = Post.builder()
+                        .title(faker.book().title())
+                        .postAuthor(memberUsers.get(new Random().nextInt(memberUsers.size())))
+//                        .replyCount(new Random().nextLong(1000))
+//                        .createdAt(faker.date().between(from, to).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
+                        .build();
+                Post savedPost = postRepository.save(post);
+                posts.add(savedPost);
+
+                PostContent postcontent = PostContent.builder()
+//                        .id(i)
+                        .post(savedPost)
+                        .content(faker.lorem().paragraph())
+                        .build();
+                postContents.add(postContentRepository.save(postcontent));
+            }
+
+            Post savedPost = postRepository.save(Post.builder()
+                    .title(faker.book().title())
+                    .postAuthor(user)
+//                        .replyCount(new Random().nextLong(1000))
+//                        .createdAt(faker.date().between(from, to).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
+                    .build());
+            posts.add(savedPost);
+
+            PostContent postcontent = PostContent.builder()
+//                        .id(i)
+                    .post(savedPost)
+                    .content(faker.lorem().paragraph())
+                    .build();
+            postContents.add(postContentRepository.save(postcontent));
+
+            // 댓글 생성
+            for (int i = 0; i < 300; i++) {
+                Post post = posts.get(new Random().nextInt(posts.size()));
+                Reply reply = Reply.builder()
+                        .content(faker.lorem().sentence())
+                        .replyAuthor(memberUsers.get(new Random().nextInt(memberUsers.size())))
+                        .replyPost(post)
+                        .build();
+                replyRepository.save(reply);
+            }
+
+            System.out.println("✓ 더미 데이터 생성 완료");
+        };
+    }
+}
